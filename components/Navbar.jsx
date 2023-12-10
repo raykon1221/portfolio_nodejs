8e99434fc1d02e79a0b318553763fff77da85883
Benjamin <tankerkzheng1@gmail.com>
2023-12-10 21:18:44 +0800

    fixed error hehe

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import Navbar from "./Navbar";
 
-const Layout = ({ children }) => {
-    return(
-        <div className="min-h-screen mx-auto ">
-            <Navbar />
-            <main className=" pl-6 pr-6  md:pl-12 md:pr-12 lg:pr-12 lg:pl-12">{children}</main>
+const Navbar = () => {
+  return (
+    <nav className="scrolled-header sticky top-0 left-0 w-full border-b border-[#4d4e53] shadow-sm text-xl bg-black z-10 mx-auto">
+      <div className="px-12 h-24 flex justify-between items-center text-white relative">
+        <a href="#" className="md:inline hidden">
+          Xin Rou
+        </a>
+        <a href="#" className="md:hidden">
+          X.R.
+        </a>
+        <div className="flex justify-end space-x-2">
+          <a href="#aboutMe" className="p-4">
+            About
+          </a>
+          <a href="#projects" className="p-4">
+            Projects
+          </a>
+          <a href="#contact" className="p-4">
+            Contact
+          </a>
         </div>
-    );
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Layout;
+export default Navbar;
--- components/TabButton.jsx
+++ components/TabButton.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 //1c
 const TabButton = ({ active, selectTab, children }) => {
-const buttonClass = active 
-    ? "text-gray-100 border-b border-purple-500" 
+  const buttonClass = active
+    ? "text-gray-100 border-b border-purple-500"
     : "text-gray-400";
-    return(
-        <button onClick={selectTab}>
-            <p className={`mr-5 font-semibold hover:text-gray-100 ${buttonClass}`}>{children}</p>
-        </button>
-    );
+  return (
+    <button onClick={selectTab}>
+      <p className={`mr-5 font-semibold hover:text-gray-100 ${buttonClass}`}>
+        {children}
+      </p>
+    </button>
+  );
 };
 export default TabButton;
